d72d22ea668303aee243bafe205ae939fc3c3bba
Mudit Rajput <muditrajputpersonal@gmail.com>
2024-06-28 02:25:44 +0530

    'post1stYear'

--- src/app/component/AdCode.jsx
+++ src/app/component/AdCode.jsx
@@ -2,29 +2,27 @@
 import { useEffect } from 'react';
 
 const AdCode = () => {
-  
-  useEffect(() => {
+    useEffect(() => {
+        // Check if window.adsbygoogle is defined and push an empty object to initialize ads
+        if (typeof window !== "undefined" && window.adsbygoogle) {
+            try {
+                (window.adsbygoogle = window.adsbygoogle || []).push({});
+            } catch (e) {
+                console.error('Adsbygoogle push error:', e);
+            }
+        }
+    }, []);
 
-    try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }
-
-    catch (e) {
-            console.log(e);
-    }
-
-},[]);
-
-  return (
-    <div>
-    <ins class="adsbygoogle"
- style={{display:"block"}}
- data-ad-client="ca-pub-8884977910314227"
- data-ad-slot="4416077835"
- data-ad-format="auto"
- data-full-width-responsive="true"></ins>
-</div>
-  );
+    return (
+        <div>
+            <ins className="adsbygoogle"
+                style={{ display: 'block' }}
+                data-ad-client="ca-pub-8884977910314227"
+                data-ad-slot="4416077835"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+        </div>
+    );
 };
-{/* <div className="container mx-auto text-center" style={{ minHeight: '1px', minWidth: '1px' }} aria-hidden={true}> */}
+
 export default AdCode;
---
+++ src/app/post/1stYear/Chemistry/page.jsx
@@ -0,0 +1,183 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { FaTelegram } from "react-icons/fa";
+import chemistry from "../../../PostImage/chemistry.png";
+import AdCode from "../../../component/AdCode";
+const Rts = () => {
+  const pageTitle =
+    "Engineering Chemistry Aktu Quantum pdf 1st year| B.tech 1st year aktu quantum pdf ";
+  const pageDescription =
+    "Get the latest Engineering Chemistry Aktu Quantum PDF for B.Tech 1st year. Access question patterns, syllabus, and more.";
+  const pageKeywords =
+    "Engineering Chemistry Aktu Quantum, Aktu Quantum, B.Tech 1st Year, PDF, Syllabus, aktu , aktu result, aktu one view , Aktu 1st year quanutm pdf free";
+
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription} />
+        <meta name="keywords" content={pageKeywords} />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:description" content={pageDescription} />
+        <meta property="og:image" content="/path/to/your/image.jpg" />
+        <meta
+          property="og:url"
+          content="http://aktu-quantum.online/path/to/your/page"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={pageDescription} />
+        <meta name="twitter:image" content="/path/to/your/image.jpg" />
+      </Head>
+      <div className="container mx-auto p-4">
+        <div className="flex flex-col lg:flex-row">
+          <div className="main-content lg:w-3/4 p-4">
+            <div className=" rounded-md shadow-sm shadow-gray-700 p-7">
+              <div className="mb-10">
+                <h1 className="font-bold text-3xl mb-2">
+                  Engineering Chemistry Aktu Quantum pdf| B.tech 1st year aktu quantum pdf
+                </h1>
+              </div>
+              <div className="mb-6">
+                <div className="flex justify-center items-center mb-3">
+                  <Image
+                    src={chemistry}
+                    alt="Syllabus of Engineering Chemistry Aktu Quantum"
+                    className="border-2 border-black"
+                  />
+                </div>
+                <AdCode />
+                <p>
+                  Hello mates, how are you, so the exam is near and you are
+                  concerned about your exam, especially Engineering Chemistry. Don't worry!
+                  In this post, I will share the Engineering Chemistry Aktu Quantum pdf with
+                  you. From this Aktu quantum pdf, you will easily get the ideas
+                  of question patterns from the last 4-5 years. Most
+                  importantly, this pdf is the latest and covers all the
+                  syllabus as per AKTU syllabus. So without wasting any time,
+                  let's see how you can get the Latest Engineering Chemistry Aktu Quantum
+                  pdf for B-Tech in 1st year.
+                </p>
+                <Link href="https://t.me/freeAktuQuantum1">
+                  <div className="flex justify-center items-center mt-5 ">
+                    <FaTelegram className=" text-3xl sm:text-6xl" />{" "}
+                    <p className="px-3 font-semibold text-orange-500  underline">
+                      Join our telegram
+                    </p>
+                  </div>
+                </Link>
+              </div>
+              <AdCode />
+              <div>
+                <h2 className="text-xl font-semibold mb-4 mt-1">
+                  How to access the Latest Engineering Chemistry Aktu Quantum pdf
+                </h2>
+                <p>
+                  You need to follow some steps to get access to the Engineering Chemistry
+                  Quantum.
+                </p>
+                <p className="text-lg font-semibold mb-1 mt-1">
+                  Step 1: Go to our website.
+                </p>
+                <p>
+                  You can directly redirect{" "}
+                  <a
+                    href="http://aktu-quantum.online/"
+                    className="text-orange-600 font-semibold"
+                  >
+                    from here
+                  </a>{" "}
+                  or type in aktu-quantum.online in the browser URL.
+                </p>
+                <p className="text-lg font-semibold mb-1 mt-1">
+                  Step 2: Access by year-wise button
+                </p>
+                <p>
+                  After clicking on the official website, you are redirected to
+                  our official website, where you see the years button. As
+                  Engineering Chemistry comes under 1 year, you can click on the 1-year
+                  button. After clicking on the 1-year button, the pdf section
+                  will be refreshed, and all quantum is of 1 years. Scroll down
+                  and find the Engineering Chemistry pdf, click on it, and access it
+                  directly
+                </p>
+                <p className="text-lg font-semibold mb-1 mt-1">
+                  Step 3: Access by searching.
+                </p>
+                <p>
+                  If you don't want to scroll down, no problem. On the same
+                  page, you see the search bar, click on it, and type Engineering Chemistry
+                  . Simply type the first word of any Aktu quantum you want to
+                  find. After writing, click on the search button, and as you
+                  click on it, below it you see the Engineering Chemistry aktu quantum pdf.
+                  Click on it and access it easily.These are all the steps you
+                  need to follow to get access to Engineering Chemistry Aktu quantum pdf.
+                </p>
+                <p>
+                  These are all the steps you need to follow to get access to
+                  Engineering Chemistry Aktu Quantum pdf.
+                </p>
+                <h3 className="text-lg font-semibold mb-1 mt-2">
+                  Syllabus of Engineering Chemistry Aktu Quantum pdf.
+                </h3>
+                <p>
+                  In Engineering Chemistry pdf, there are a total of 5 units as per the Aktu
+                  syllabus of Engineering Chemistry.
+                  <br />{" "}
+                  <span className="font-semibold">
+                    Unit 1: : Atomic &Molecular Structure
+                  </span>{" "}
+                  <br />
+                  <span className="font-semibold">
+                    Unit 2: Spectroscopic Techniques & Applications
+                  </span>{" "}
+                  <br />
+                  <span className="font-semibold">
+                    Unit 3: Electrochemistry
+                  </span>{" "}
+                  <br />
+                  <span className="font-semibold">
+                    Unit 4: Water Technology , Fuel And Combustion
+                  </span>{" "}
+                  <br />
+                  <span className="font-semibold">
+                    Unit 5: : Materials Chemistry
+                  </span>{" "}
+                  <br />
+                </p>
+                <p className="text-lg font-semibold mb-1 mt-1">
+                  What if I can't get the Aktu quantum?
+                </p>
+                <p>
+                  In the header section, you get the pdf section, click on that
+                  and fill out the form. You need to enter some details like pdf
+                  name, year, and semester with the code, and click on submit.
+                  Within 1 hour, we upload the pdf and you get access to that
+                  Aktu quantum pdf..
+                </p>
+                <p className="text-lg font-semibold mb-1 mt-1">Conclusion</p>
+                <p>
+                In this post, we learned how to access Engineering Chemistry Aktu Quantum pdf for B-tech 1st year in just a few steps. I hope you like the post. There is a button below from which you can get access to the aktu quantum series for all B.tech.
+
+
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="sidebar lg:w-1/4 p-4 bg-gray-100">
+            <AdCode />
+            <ul className="text-orange-500 font-semibold">
+              <Link href="/post/rts">Real time Aktu Quantum pdf</Link>
+            </ul>
+            <AdCode />
+            {/* <h2 className="text-xl font-semibold mb-4">Sidebar Content</h2> */}
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Rts;
